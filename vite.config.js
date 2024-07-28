8dbb4760f407b997e7fa94ba958be091fc83b936
pipypupa <zong8729@gmail.com>
2024-07-28 19:26:09 +0300

    add src

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+export default defineConfig(({ command }) => {
+    return {
+    define: {
+    [command === "serve" ? "global" : "_global"]: {},
+    },
+    root: "src",
+    build: {
+    sourcemap: true,
+    
+    rollupOptions: {
+    input: glob.sync("./src/*.html"),
+    output: {
+    manualChunks(id) {
+    if (id.includes("node_modules")) {
+    return "vendor";
+    }
+    },
+    entryFileNames: "commonHelpers.js",
+    },
+    },
+    outDir: "../dist",
+    },
+    plugins: [injectHTML(), FullReload(["./src/**/**/**.html"])],
+    };
+    });
